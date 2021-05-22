1f13f57132175817a3d69a61321fbd054c4ee7b6
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-22 14:07:50 -0600

    Add pagination method to controller and spec

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     # binding.pry
-    merchants = Merchant.all
+    merchants = Merchant.pagination(params.fetch(:per_page, 20).to_i, params.fetch(:page, 1).to_i)
     render json: MerchantSerializer.new(merchants)
   end
 
--- spec/request/api/v1/merchants/index_spec.rb
+++ spec/request/api/v1/merchants/index_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Merchants API", type: :request do
 
       merchants = JSON.parse(response.body, symbolize_names: true)
 
-      expect(merchants[:data].count).to eq(50)
+      expect(merchants[:data].count).to eq(20)
 
       merchants[:data].each do |merchant|
         expect(merchant).to have_key(:id)
@@ -23,6 +23,59 @@ RSpec.describe "Merchants API", type: :request do
         expect(merchant[:attributes]).to have_key(:name)
         expect(merchant[:attributes][:name]).to be_a(String)
       end
+    end
+
+    it 'can get all merchants, a maximum of 20 at a time' do
+      get '/api/v1/merchants'
+      first_20 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=1'
+      expect(response).to be_successful
+
+      page_1 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(first_20[:data].count).to eq(20)
+      expect(first_20[:data]).to eq(page_1[:data])
+    end
+
+    it 'can get next page of 20 merchants' do
+      get '/api/v1/merchants?page=1'
+      page_1 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=2'
+
+      expect(response).to be_successful
+
+      page_2 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(page_2[:data].count).to eq(20)
+
+      page_1_first_id = page_1[:data].first[:id].to_i
+      page_2_first_id = page_1_first_id + 20
+
+      expect(page_2[:data].first[:id].to_i).to eq(page_2_first_id)
+    end
+
+    it 'can get page 3 of merchants' do
+      get '/api/v1/merchants?page=1'
+      page_1 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=2'
+      page_2 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=3'
+      page_3 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+
+      expect(page_3[:data].count).to eq(10)
+
+      page_1_first_id = page_1[:data].first[:id].to_i
+      page_2_first_id = page_1_first_id + 20
+      page_3_first_id = page_2_first_id + 20
+      # binding.pry
+      expect(page_3[:data].first[:id].to_i).to eq(page_3_first_id)
     end
   end
 end
