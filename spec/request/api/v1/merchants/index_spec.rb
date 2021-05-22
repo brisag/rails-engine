557fb60cb395fc9be59c784a30077cfeb659076a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-22 13:22:49 -0600

    Add find all and specific merchants + spec test

---
+++ spec/request/api/v1/merchants/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Merchants API", type: :request do
+  before :each do
+    create_list(:merchant, 50)
+  end
+
+  describe 'Merchant Index' do
+    it "get all merchants" do
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data].count).to eq(50)
+
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id].to_i).to be_an(Integer)
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to eq('merchant')
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+  end
+end
---
+++ spec/request/api/v1/merchants/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Merchants API", type: :request do
+  before :each do
+    @merchant = create(:merchant)
+  end
+
+  describe 'Merchant Show' do
+    it 'can get one merchant by its id' do
+      get "/api/v1/merchants/#{@merchant.id}"
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id].to_i).to eq(@merchant.id)
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+    end
+  end
+end
