ad9a22a4a71ce89bef0aa4e352c4f07b04e097e0
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-24 15:42:01 -0600

    Add CRUD for Items API

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,38 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :update]
+
   def index
     items = Item.pagination(params.fetch(:per_page, 20).to_i, params.fetch(:page, 1).to_i)
     render json: ItemSerializer.new(items)
   end
 
   def show
-    item = Item.find(params[:id])
-    render json: ItemSerializer.new(item)
+    # item = Item.find(params[:id])
+    render json: ItemSerializer.new(@item)
   end
+
+  def create
+    item = Item.create!(item_params)
+    render json: ItemSerializer.new(item), status: :created
+  end
+
+  def update
+    @item.update!(item_params)
+    render json: ItemSerializer.new(@item)
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
