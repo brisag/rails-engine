1a1028b493046e8893128f5b1b7d645acd624e42
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-24 17:30:46 -0600

    Fix test by adding type:request

--- spec/request/api/v1/items/edit_spec.rb
+++ spec/request/api/v1/items/edit_spec.rb
@@ -1,42 +1,42 @@
 require 'rails_helper'
-RSpec.describe 'Item API' do
+RSpec.describe 'Item API', type: :request do
   before :each do
     @item = create(:item)
   end
 
   describe 'Edit/Update Item' do
-    xit "can update an existing item" do
+    it "can update an existing item" do
       previous_item = Item.last.name
-      item_params = { name: "New Item" }
+      item_params = { unit_price: 20.22 }
       headers = {"CONTENT_TYPE" => "application/json"}
-
-      patch api_v1_item(@item.id), headers: headers, params: JSON.generate({item: item_params})
-      item = Item.find_by(id: @item.id)
+      item = Item.find_by(id: @item)
+      # binding.pry
+      patch "/api/v1/items/#{@item.id}", headers: headers, params: JSON.generate({item: item_params})
 
       expect(response).to be_successful
       expect(item.name).not_to eq(previous_item)
-      expect(item.name).to eq("New Item")
+      expect(item.name).to eq(20.22)
     end
   end
 
   describe 'sad path' do
-    xit 'bad integer id returns 404' do
+    it 'bad integer id returns 404' do
       item_params = { name: "New Item" }
       headers = {"CONTENT_TYPE" => "application/json"}
-
-      patch api_v1_item(1234554321), headers: headers, params: JSON.generate({item: item_params})
       item = Item.find_by(id: 1234554321)
+
+      patch "/api/v1/items/1234554321", headers: headers, params: JSON.generate({item: item_params})
 
       expect(response.status).to eq 404
     end
 
-    xit 'bad merchant id returns 404' do
+    it 'bad merchant id returns 404' do
       previous_item = Item.last.name
       item_params = { merchant_id: 0 }
       headers = {"CONTENT_TYPE" => "application/json"}
+      item = Item.find_by(id: @item.id)
 
       patch "/api/v1/items/#{@item.id}", headers: headers, params: JSON.generate({item: item_params})
-      item = Item.find_by(id: @item.id)
 
       expect(response.status).to eq(404)
     end
