891f492906e0a0fd0e1114c021471f5974fb3831
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-25 16:33:47 -0600

    Add revenue model method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,33 @@ class Merchant < ApplicationRecord
   def self.search_by_name(name)
     where("name ILIKE '%#{name}%'").order(:name)
   end
+
+  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue').group(:id)
+  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity)').group(:id).first
+  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue').group(:id).order(total_revenue: :desc)
+  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').pluck('sum(invoice_items.unit_price * invoice_items.quantity)').first
+  def revenue
+    transactions
+    .where('transactions.result = ?', 'success')
+    .where('invoices.status = ?', 'shipped')
+    .pluck('sum(invoice_items.unit_price * invoice_items.quantity)')
+    .first
+  end
 end
+
+
+    # .merge(Transaction.successful)
+    # .merge(Invoice.shipped)
+    # .sum('invoice_items.unit_price * invoice_items.quantity')
+    # .where('transactions.result = ?', 'success')
+    # .where('invoices.status = ?', 'shipped')
+    # .pluck('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
+    # .first.to_float
+    # .group(:id)
+
+    # http://localhost:3000/api/v1/revenue/merchants/{{merchant_id}}
+
+
+    # transaction = successful
+    # invoice = shipped or packaged
+    # invoice_items = quantity * unit price
