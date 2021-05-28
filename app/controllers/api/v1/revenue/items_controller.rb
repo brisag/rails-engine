49fce75b7afe8e31f51b00b4f118a64b6c050adc
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-28 08:24:01 -0600

    Add edge case testing

--- app/controllers/api/v1/revenue/items_controller.rb
+++ app/controllers/api/v1/revenue/items_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::Revenue::ItemsController < ApplicationController
-  def index
-    # binding.pry
-    if params[:quantity].to_i.present?
-      render json: ItemRevenueSerializer.new(Item.ranked_by_revenue(params[:quantity]))
-    end
+#   def index
+#     # binding.pry
+#     if params[:quantity].to_i.present?
+#       render json: ItemRevenueSerializer.new(Item.ranked_by_revenue(params[:quantity]))
+#     end
+#   end
+# end
+#
+
+def index
+  if params[:quantity].nil? || params[:quantity].to_i > 0
+    render json: ItemRevenueSerializer.new(Item.ranked_by_revenue(quantity))
+  else
+    render json: { error: '400 Bad Request' }, status: :bad_request
   end
+end
+
+private
+
+def quantity
+  params.fetch(:quantity, 10).to_i
+end
 end
