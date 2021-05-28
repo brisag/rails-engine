b07a997ef8b0161ee684d22c31688294bacfff5c
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-28 06:48:43 -0600

    Fix search enpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/api/v1/merchants/find_all', to: 'api/v1/merchants/search#index'
+  get '/api/v1/items/find', to: 'api/v1/items/search#index'
   namespace :api do
     namespace :v1 do
       #merchants  index+show and merchant_items items index
@@ -16,8 +18,13 @@ Rails.application.routes.draw do
         resources :unshipped, only: [:index]
         resources :items, only: [:index]
       end
-      #Non-RESTful Search Endpoints
-
+  #     #Non-RESTful Search Endpoints Section 2
+  #     # namespace :merchants do
+      #   resources :find_all, controller: :search, only: :index
+      # end
+      # namespace :items do
+      #   resources :find, controller: :search, only: :index
+      # end
     end
   end
 end
