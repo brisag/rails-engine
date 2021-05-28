328dabfc55a6db27406e30b8515ca7812e52bc1b
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-27 21:09:12 -0600

    Add controller to find items from a specific merchant

--- app/controllers/api/v1/items_merchant_controller.rb
+++ app/controllers/api/v1/items_merchant_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ItemsMerchantController < ApplicationController
   def index
-    # binding.pry
     items = Item.find(params[:item_id])
     render json: MerchantSerializer.new(items.merchant)
   end
