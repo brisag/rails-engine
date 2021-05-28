8c7e7d938321e83c7d68374d1922a07cd31e27a6
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-27 21:13:22 -0600

    Add merchants wiht most revenue, unshipped revenue, and revenue methods to merchant class

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,14 +5,48 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
-  def self.search_by_name(name)
-    where("name ILIKE '%#{name}%'").order(:name)
+
+  # def self.search_criteria(name)
+  #   where("name ILIKE ?", "%#{name}%")
+  #   .order(:name)
+  # end
+
+  # def self.search_criteria(name)
+  #   where("name ilike ?", "%#{name}%")
+  #   .order(:name)
+  # end
+
+  def self.merchants_with_most_revenue(quantity)
+    # binding.pry
+    select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
+    .joins(:transactions)
+    .where('transactions.result = ?', 'success')
+    .where('invoices.status = ?', 'shipped')
+    .group(:id)
+    .order(total_revenue: :desc)
+    .limit(quantity)
   end
 
-  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue').group(:id)
-  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity)').group(:id).first
-  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue').group(:id).order(total_revenue: :desc)
-  # Merchant.joins(:transactions).where('transactions.result = ?', 'success').where('invoices.status = ?', 'shipped').pluck('sum(invoice_items.unit_price * invoice_items.quantity)').first
+  def self.unshipped_revenue(quantity)
+    joins(:invoices)
+    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as potential_revenue')
+    .group(:id)
+    .where(status: :packaged)
+    .order(potential_revenue: :desc)
+    .limit(quantity)
+  end
+
+  # def self.merchants_with_most_revenue(quantity)
+  #   select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
+  #   .joins(:transactions)
+  #   .where('transactions.result = ?', 'success')
+  #   .where('invoices.status = ?', 'shipped')
+  #   .group(:id)
+  #   .order(total_revenue: :desc)
+  #   .limit(quantity)
+  # end
+
+
   def revenue
     transactions
     .where('transactions.result = ?', 'success')
@@ -21,20 +55,3 @@ class Merchant < ApplicationRecord
     .first
   end
 end
-
-
-    # .merge(Transaction.successful)
-    # .merge(Invoice.shipped)
-    # .sum('invoice_items.unit_price * invoice_items.quantity')
-    # .where('transactions.result = ?', 'success')
-    # .where('invoices.status = ?', 'shipped')
-    # .pluck('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
-    # .first.to_float
-    # .group(:id)
-
-    # http://localhost:3000/api/v1/revenue/merchants/{{merchant_id}}
-
-
-    # transaction = successful
-    # invoice = shipped or packaged
-    # invoice_items = quantity * unit price
