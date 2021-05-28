8664f15f2be213853d851b623f11518b70e942ec
Brisa Garcia <brisagarciaglz@gmail.com>
2021-05-27 21:14:49 -0600

    refactor and remove uncommented code

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,53 +5,28 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
-
-  # def self.search_criteria(name)
-  #   where("name ILIKE ?", "%#{name}%")
-  #   .order(:name)
-  # end
-
-  # def self.search_criteria(name)
-  #   where("name ilike ?", "%#{name}%")
-  #   .order(:name)
-  # end
-
-  def self.merchants_with_most_revenue(quantity)
-    # binding.pry
-    select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
-    .joins(:transactions)
-    .where('transactions.result = ?', 'success')
-    .where('invoices.status = ?', 'shipped')
-    .group(:id)
-    .order(total_revenue: :desc)
-    .limit(quantity)
-  end
-
-  def self.unshipped_revenue(quantity)
-    joins(:invoices)
-    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as potential_revenue')
-    .group(:id)
-    .where(status: :packaged)
-    .order(potential_revenue: :desc)
-    .limit(quantity)
-  end
-
-  # def self.merchants_with_most_revenue(quantity)
-  #   select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
-  #   .joins(:transactions)
-  #   .where('transactions.result = ?', 'success')
-  #   .where('invoices.status = ?', 'shipped')
-  #   .group(:id)
-  #   .order(total_revenue: :desc)
-  #   .limit(quantity)
-  # end
-
-
   def revenue
     transactions
     .where('transactions.result = ?', 'success')
     .where('invoices.status = ?', 'shipped')
     .pluck('sum(invoice_items.unit_price * invoice_items.quantity)')
     .first
+  end
+
+  class << self
+    def search_criteria(name)
+      where("name ILIKE ?", "%#{name}%")
+      .order(:name)
+    end
+
+    def merchants_with_most_revenue(quantity)
+      select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue')
+      .joins(:transactions)
+      .where('transactions.result = ?', 'success')
+      .where('invoices.status = ?', 'shipped')
+      .group(:id)
+      .order(total_revenue: :desc)
+      .limit(quantity)
+    end
   end
 end
